e96c3e1acbd18e878cd3a6b6253226d21d9561cc
MagdalenaES83 <84572667+MagdalenaES83@users.noreply.github.com>
2021-11-16 21:02:42 +0100

    Main component Ok
    Modal for add pic connected,

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,21 +1,94 @@
-import React from "react";
-
-import { Container, Col, Row, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { AiOutlinePicture } from "react-icons/ai";
+import { ImCross } from "react-icons/im";
+import { GrEmoji } from "react-icons/gr";
+import {
+  Container,
+  Col,
+  Row,
+  Form,
+  Card,
+  Modal,
+  Button,
+} from "react-bootstrap";
 
 export default function Chat() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+
   return (
     <Container className="ChatBody">
       <Row>
-        <Col id="body"></Col>
+        <Col id="historyChat">
+          <Col md={5} className="my-2">
+            <Card className="my-1">
+              <Card.Body>
+                <Card.Text>Some text message </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={7} className="my-2">
+            <Card className="my-1">
+              <Card.Body>
+                <Card.Text>Some another message </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={9} className="my-2">
+            <Card className="my-1">
+              <Card.Body>
+                <Card.Text>Some text message </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Col>
       </Row>
       <Row>
-        <Col>
+        <Col md={12}>
           <Form>
             <Form.Group controlId="exampleForm.ControlTextarea1">
-              <Form.Label>.............😃🎉🐱‍👤(☞ﾟヮﾟ)☞......................</Form.Label>
-              <Form.Control as="textarea" placeholder="Send a message" rows={2} />
+              <Form.Control
+                as="textarea"
+                placeholder="Send a message"
+                rows={2}
+              />
             </Form.Group>
           </Form>
+          <Col md={12} id="addPic">
+            <Button onClick={handleShow}>
+              Add + <AiOutlinePicture />
+            </Button>
+
+            <Modal
+              show={show}
+              onHide={handleClose}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header >
+                <Modal.Title>Add picture</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                ...
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  <ImCross />
+                </Button>
+                
+              </Modal.Footer>
+            </Modal>
+
+            
+
+            <Button
+            //onClick={handleShow}
+            >
+              Add + <GrEmoji />
+            </Button>
+          </Col>
         </Col>
       </Row>
     </Container>
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import User from './User'
 import Chat from './Chat'
-import { Row, Col} from 'react-bootstrap'
+import { Row, Col, Navbar} from 'react-bootstrap'
 
 export default function Main() {
   return (
-    <div>
+    <>
+    
+  <Navbar bg="light"><Col md={4}>
+    <Navbar.Brand href="#home"><img
+        alt=""
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXjEvvLvYr01bE7eQ7QaFsEDEMd3eNzeBH3w&usqp=CAU"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      /> 🐞</Navbar.Brand></Col>
+      <Col md={4} > </Col>
+      <Col md={4} ><img
+        alt=""
+        src="https://media.istockphoto.com/photos/seven-spotted-ladybug-on-a-white-background-picture-id117485656?b=1&k=20&m=117485656&s=170667a&w=0&h=tqyVgEFteWRyebzvoFAf3IZyuoxdnXVejQR5VBPwczs="
+        width="40"
+       
+        className="d-inline-block align-top"
+      /> </Col>
+      
+
+  </Navbar>
+  
+  
+
       <Row>
         <Col md={4}>
           <User />
@@ -14,6 +37,6 @@ export default function Main() {
           <Chat />
         </Col>
       </Row>
-    </div>
+    </>
   )
 }
