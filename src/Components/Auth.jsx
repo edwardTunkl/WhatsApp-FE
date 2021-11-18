bee43f8dd3f519a24508e3911c87771decc84ce5
emanuele <emanuelefurlani.go@gmail.com>
2021-11-18 20:12:14 +0100

    upload all the last part of auth

--- src/Components/Auth.jsx
+++ src/Components/Auth.jsx
@@ -1,3 +1,4 @@
+import { set } from "date-fns/esm";
 import React from "react";
 import { useState, useEffect } from "react";
 import { Navigate } from "react-router-dom";
@@ -6,43 +7,52 @@ import Main from "./Main";
 function ProtectedRoute() {
   const [isAuthenticated, setAuthenticated] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("token"));
-
+  const [fetchingUser, setFetchingUser] = useState(true);
   const [userInfo, setUserInfo] = useState(false);
 
-  useEffect(async () => {
-    if (token) {
-      try {
-        let req = await fetch(process.env.REACT_APP_BE_URL + "/users/me", {
-          method: "GET",
-          headers: { Authorization: localStorage.getItem("token") },
-        });
-        if (req.ok) {
-          let userInfo = await req.json();
-          if (userInfo) {
-            setUserInfo(true);
-          } else {
-            return;
-          }
-        } else {
-          throw new Error(req.statusText);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-      if (userInfo) {
-        setAuthenticated(true);
-      }
-    }
-  }, [userInfo]);
-
   useEffect(() => {
-    if (isAuthenticated) {
-      return () => {};
+    if (token) {
+      const auth = async () => {
+        try {
+          let req = await fetch(process.env.REACT_APP_BE_URL + "/users/me", {
+            method: "GET",
+            headers: { Authorization: localStorage.getItem("token") },
+          });
+          if (req.ok) {
+            let userInfo = await req.json();
+            if (userInfo) {
+              setAuthenticated(true);
+            } else {
+            }
+          } else {
+            throw new Error(req.statusText);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      auth();
+      console.log(isAuthenticated);
+      return () => {
+        setFetchingUser(false);
+        console.log("look here");
+      };
     }
-  });
+  }, [token]);
+
+  // check if a user is auth,if(auth) wait for every code to run, then redirect
+
   console.log(isAuthenticated);
 
-  return <>{isAuthenticated ? <Main /> : <Navigate to="/login" />}</>;
+  return (
+    <>
+      {isAuthenticated && fetchingUser === false ? (
+        <Main />
+      ) : (
+        <Navigate to="/login" />
+      )}
+    </>
+  );
 }
 
 export default ProtectedRoute;
